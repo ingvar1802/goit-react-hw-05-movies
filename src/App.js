b1027184b3f3c850df3909815860b19783c4d257
Ihor Nadieien <ingwar1802@gmail.com>
2021-01-12 18:49:26 +0200

    add app

---
+++ src/App.js
@@ -0,0 +1,34 @@
+import { lazy, Suspense } from 'react';
+import { Switch, Route } from 'react-router-dom';
+import AppBar from './components/AppBar/AppBar';
+import Container from './components/Container/Container';
+import Loader from './components/Loader/Loader';
+
+const HomeView = lazy(() => import('./views/HomeView/HomeView' /*webpackChunkName: "HomeView"*/));
+const MoviesView = lazy(() => import('./views/MoviesView' /* webpackChunkName: "MoviesView" */));
+const MovieDetailedView = lazy(() => import('./views/MovieDetailedView/MovieDetailedView' /* webpackChunkName: "MovieDetailedView" */));
+
+
+
+export default function App() {
+    return (
+        <Container>
+            <AppBar />
+            <Suspense fallback={<Loader />}>
+                <Switch>
+                    <Route path="/" exact>
+                        <HomeView />
+                    </Route>
+
+                    <Route path="/movies" exact>
+                        <MoviesView />
+                    </Route>
+
+                    <Route path="/movies/:movieId">
+                        <MovieDetailedView />
+                    </Route>
+                </Switch>
+            </Suspense>
+        </Container>
+    );
+}
---
+++ src/components/Navigation/Navigation.js
@@ -0,0 +1,24 @@
+import { NavLink } from 'react-router-dom';
+import styles from './Navigation.module.css';
+
+const Navigation = () =>
+    <nav>
+        <NavLink
+            to="/"
+            exact
+            className={styles.link}
+            activeClassName={styles.activeLink}
+        >
+            HomePage
+            </NavLink>
+        
+        <NavLink
+            to="/movies"
+            className={styles.link}
+            activeClassName={styles.activeLink}
+        >
+            MoviesPage
+        </NavLink>
+    </nav>;
+
+export default Navigation;
